86f75cdafbd68eabe5d92726cb1e5c5b2a135f63
Khandaker Akash <akash.cse10@gmail.com>
2018-04-21 01:47:19 +0600

    Studio front-part done

--- resources/views/partials/studio.blade.php
+++ resources/views/partials/studio.blade.php
@@ -2,18 +2,22 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-lg-7">
-                <div class="studio__left">
-                    <img src="{{asset('img/concept.jpg')}}" alt="Conceptual Image One" class="img-responsive studio__left--bg">
+                <div class="studio__left-top">
+                    <img src="{{asset('img/studio/concept-img-1.jpg')}}" alt="Conceptual Image One" class="img-responsive studio__left-top--bg">
+                </div>
+                <div class="studio__left-top--btn-plus">
+                    <i class="fa fa-plus fa-2x"></i>
                 </div>
             </div>
+
             <div class="col-lg-5">
-                <div class="studio__right">
+                <div class="studio__right-top">
                     <blockquote class="blockquote">
                         <p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
                         <footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
                     </blockquote>
 
-                    <p class="paragraph u-justify-text studio__right--text">
+                    <p class="paragraph u-justify-text studio__right-top--text">
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis culpa debitis, delectus dolorem, doloribus, error eum eveniet ex maxime modi possimus quas qui quia quos similique vel vero. Accusamus atque culpa cum cumque est facilis id ipsa, mollitia.
                     </p>
                     <p class="paragraph u-justify-text studio__right--text">
@@ -26,5 +30,59 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div><!-- STUDIO TOP SECTION -->
+
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="studio__left-middle">
+                    <blockquote class="blockquote">
+                        <p class="mb-0">Lorem ipsum dolor sit amet.</p>
+                        <footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
+                    </blockquote>
+
+                    <p class="paragraph u-justify-text studio__left-middle--text">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis culpa debitis, delectus dolorem, doloribus, error eum eveniet ex maxime modi. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    </p>
+                    <a href="#" class="btn-text">Learn more &rarr;</a>
+                </div>
+            </div>
+
+            <div class="col-lg-6">
+                <div class="studio__right-middle">
+                    <img src="{{asset('img/studio/conceptual-img-2.jpg')}}" alt="Conceptual Image Two" class="img-responsive studio__right-middle--bg">
+                </div>
+                <div class="studio__right-middle--btn-plus">
+                    <i class="fa fa-plus fa-2x"></i>
+                </div>
+            </div>
+        </div>
+    </div><!-- STUDIO MIDDLE SECTION -->
+
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="studio__left-bottom">
+                    <img src="{{asset('img/studio/conceptual-img-3.jpg')}}" alt="Conceptual Image Two" class="img-responsive studio__left-bottom--bg">
+                </div>
+                <div class="studio__left-bottom--btn-plus">
+                    <i class="fa fa-plus fa-2x"></i>
+                </div>
+            </div>
+
+            <div class="col-lg-6">
+                <div class="studio__right-bottom">
+                    <blockquote class="blockquote">
+                        <p class="mb-0">Lorem ipsum dolor sit amet.</p>
+                        <footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
+                    </blockquote>
+
+                    <p class="paragraph u-justify-text studio__right-bottom--text">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis culpa debitis, delectus dolorem, doloribus, error eum eveniet ex maxime modi. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    </p>
+                    <a href="#" class="btn-text">Learn more &rarr;</a>
+                </div>
+            </div>
+        </div>
+    </div><!-- STUDIO BOTTOM SECTION -->
 </section>
